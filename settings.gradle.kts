e4940672e5c87ae0b1560ada1e6ca43cef9d7c9d
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-05-11 16:58:25 +0200

    Publish build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,17 @@
-rootProject.name = "java-module-testing"
+plugins {
+    id("com.gradle.enterprise") version "3.10"
+}
 
 dependencyResolutionManagement {
     repositories.mavenCentral()
+}
+
+rootProject.name = "java-module-testing"
+
+gradleEnterprise {
+    buildScan {
+        publishAlways()
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
 }
