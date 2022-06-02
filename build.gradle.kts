c9764cdbfb3ea5b266f3477c2ebf4248c91eab3c
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-06-02 12:12:11 +0200

    Add more descriptive metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,21 +16,52 @@ dependencies {
     testImplementation("org.gradle.exemplar:samples-check:1.0.0")
 }
 
+val pluginId = "de.jjohannes.java-module-testing"
+val pluginClass = "de.jjohannes.gradle.moduletesting.JavaModuleTestingPlugin"
+val pluginName = "Java Module Testing Gradle Plugin"
+val pluginDescription = "A plugin to test Java Modules (whitebox and blackbox) without the hassle."
+val pluginBundleTags = listOf("java", "modularity", "jigsaw", "jpms", "testing")
+val pluginGitHub = "https://github.com/jjohannes/java-module-testing"
+
 gradlePlugin {
     plugins {
         create(project.name) {
-            id = "de.jjohannes.java-module-testing"
-            implementationClass = "de.jjohannes.gradle.moduletesting.JavaModuleTestingPlugin"
-            displayName = "Java Module Testing"
-            description = "A plugin to test Java Modules (whitebox and blackbox) without the hassle."
+            id = pluginId
+            implementationClass = pluginClass
+            displayName = pluginName
+            description = pluginDescription
         }
     }
 }
 
 pluginBundle {
-    website = "https://github.com/jjohannes/java-module-testing"
-    vcsUrl = "https://github.com/jjohannes/java-module-testing"
-    tags = listOf("java", "modularity", "jigsaw", "jpms", "testing")
+    website = pluginGitHub
+    vcsUrl = pluginGitHub
+    tags = pluginBundleTags
+}
+
+publishing {
+    publications.withType<MavenPublication>().all {
+        pom.name.set(pluginName)
+        pom.description.set(pluginDescription)
+        pom.url.set(pluginGitHub)
+        pom.licenses {
+            license {
+                name.set("Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        pom.developers {
+            developer {
+                id.set("jjohannes")
+                name.set("Jendrik Johannes")
+                email.set("jendrik@onepiece.software")
+            }
+        }
+        pom.scm {
+            url.set(pluginGitHub)
+        }
+    }
 }
 
 tasks.test {
