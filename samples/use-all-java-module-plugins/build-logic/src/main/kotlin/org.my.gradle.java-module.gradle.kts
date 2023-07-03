99eef6b067dfdfc101e32f518ea50dfabbe0046e
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-07-03 09:04:34 +0200

    Adjust samples to rely on the new conventions

--- samples/use-all-java-module-plugins/build-logic/src/main/kotlin/org.my.gradle.java-module.gradle.kts
+++ samples/use-all-java-module-plugins/build-logic/src/main/kotlin/org.my.gradle.java-module.gradle.kts
@@ -13,22 +13,19 @@ javaModuleTesting.whitebox(
     }
 ) {
     requires.add("org.junit.jupiter.api")
-    opensTo.add("org.junit.platform.commons")
 }
 
-javaModuleTesting.blackbox(
-    testing.suites.create<JvmTestSuite>("integtest") {
-        useJUnitJupiter("")
-        testType.set("blackbox")
-        dependencies {
-            implementation(project.dependencies.platform(project(":platform")))
-        }
-        targets.all {
-            testTask { jvmArgs("-Dorg.slf4j.simpleLogger.defaultLogLevel=error") }
-        }
-        tasks.check { dependsOn(targets) }
+testing.suites.create<JvmTestSuite>("integtest") {
+    useJUnitJupiter("")
+    testType.set("blackbox")
+    dependencies {
+        implementation(project.dependencies.platform(project(":platform")))
     }
-)
+    targets.all {
+        testTask { jvmArgs("-Dorg.slf4j.simpleLogger.defaultLogLevel=error") }
+    }
+    tasks.check { dependsOn(targets) }
+}
 
 dependencies {
     implementation(platform(project(":platform")))
--- samples/use-with-test-fixtures/build-logic/src/main/kotlin/org.my.gradle.java-module.gradle.kts
+++ samples/use-with-test-fixtures/build-logic/src/main/kotlin/org.my.gradle.java-module.gradle.kts
@@ -12,18 +12,15 @@ javaModuleTesting.whitebox(
     }
 ) {
     requires.add("org.junit.jupiter.api")
-    opensTo.add("org.junit.platform.commons")
     requires.add("org.my.lib.test.fixtures")
 }
 
-javaModuleTesting.blackbox(
-    testing.suites.create<JvmTestSuite>("integtest") {
-        useJUnitJupiter()
-        testType.set(TestSuiteType.INTEGRATION_TEST)
-        targets.all { testTask { testLogging.showStandardStreams = true } }
-        tasks.check { dependsOn(targets) }
-    }
-)
+testing.suites.create<JvmTestSuite>("integtest") {
+    useJUnitJupiter()
+    testType.set(TestSuiteType.INTEGRATION_TEST)
+    targets.all { testTask { testLogging.showStandardStreams = true } }
+    tasks.check { dependsOn(targets) }
+}
 
 dependencies {
     implementation(platform("com.fasterxml.jackson:jackson-bom:2.13.2"))
