b13c9046a802a16d7a6ff5f474d751c6106ecc95
Benedikt Ritter <beneritter@gmail.com>
2025-06-05 16:30:00 +0200

    Update cross version tests (#110)

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ pluginPublishConventions {
 
 testing.suites.named<JvmTestSuite>("test") {
     useJUnitJupiter()
-    listOf("7.4", "7.6.4", "8.0.2").forEach { gradleVersionUnderTest ->
+    listOf("7.4", "7.6.5", "8.0.2").forEach { gradleVersionUnderTest ->
         targets.register("test${gradleVersionUnderTest}") {
             testTask {
                 group = LifecycleBasePlugin.VERIFICATION_GROUP
