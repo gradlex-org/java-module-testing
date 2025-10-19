56dad54527e228727d52817dbf58abb08b22abdf
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-10-19 12:34:07 +0200

    Fix included build location

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-pluginManagement { includeBuild("../plugin-publish-conventions") }
+pluginManagement { includeBuild("build/plugin-publish-conventions") }
 
 plugins { id("org.gradlex.internal.gradlex-build-conventions") version "0.7" }
 
