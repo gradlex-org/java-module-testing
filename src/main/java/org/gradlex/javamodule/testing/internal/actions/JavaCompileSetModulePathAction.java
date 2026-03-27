2b9a5871d3b77b6fa373f52b68e9235117a85b13
Jendrik Johannes <jendrik.johannes@gmail.com>
2026-03-27 14:55:10 +0100

    Use reflective access for JavaModuleDetector methods (#173)

--- src/main/java/org/gradlex/javamodule/testing/internal/actions/JavaCompileSetModulePathAction.java
+++ src/main/java/org/gradlex/javamodule/testing/internal/actions/JavaCompileSetModulePathAction.java
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.gradlex.javamodule.testing.internal.actions;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
@@ -31,10 +32,22 @@ public abstract class JavaCompileSetModulePathAction implements Action<Task>, De
 
         // Since for Gradle this sources set does not look like a module, we have to define the module path ourselves
         compilerArgs.add("--module-path");
-        compilerArgs.add(getJavaModuleDetector()
-                .inferModulePath(true, classpathAndModulePath)
-                .getAsPath());
-        javaCompile.setClasspath(getJavaModuleDetector().inferClasspath(true, classpathAndModulePath));
+        compilerArgs.add(infer("ModulePath", classpathAndModulePath).getAsPath());
+        javaCompile.setClasspath(infer("Classpath", classpathAndModulePath));
         javaCompile.getOptions().setCompilerArgs(compilerArgs);
+    }
+
+    /**
+     * Use reflective access for JavaModuleDetector methods so that it does not matter if JavaModuleDetector is a
+     * class (<9.5.0) or an interface (9.5.0+).
+     */
+    private FileCollection infer(String pathType, FileCollection classpathAndModulePath) {
+        try {
+            Method inferModulePath =
+                    JavaModuleDetector.class.getDeclaredMethod("infer" + pathType, boolean.class, FileCollection.class);
+            return (FileCollection) inferModulePath.invoke(getJavaModuleDetector(), true, classpathAndModulePath);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
